c4de4de48dc9b6841fe161b9dd7d7070fa3e8d6c
JoAn <joannacalinska@gmail.com>
2018-04-05 22:28:21 +0200

    day2-part3

--- app/zadanieDnia.js
+++ app/zadanieDnia.js
@@ -1,1 +1,26 @@
-//Twój kod
+const fs = require('fs');
+
+fs.readFile('./data/zadanieDnia/test.txt', 'utf8', (err, data) => {
+    if(err === null) {
+        let text = '';
+        let array = data.split("");
+
+        array.forEach((val,i) => {
+            if(i % 2 === 0) {
+                text += val.toUpperCase()
+            } else {
+                text += val.toLowerCase()
+            }
+        });
+
+        fs.writeFile('./data/zadanieDnia/test.txt', text, err => {
+            if(err === null) {
+                console.log('U mnie działa!');
+            } else {
+                console.log('cos w writeFile poszlo nie tak', err);
+            }
+        })
+    } else {
+        console.log('cos w readFile poszlo nie tak', err);
+    }
+});
